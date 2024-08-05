d4bd94fe131fa6159016a69030497ff467bea02c
Michael O'Boyle <michael@oboyle.co>
2024-08-05 16:50:32 -0400

    working without redis

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,57 +1,71 @@
 # app/controllers/weather_controller.rb
+
 class WeatherController < ApplicationController
   def show
-    if params[:address].present?
-      address = params[:address]
-      zip_code = geocode_address(address)
+    address = params[:address]
+    Rails.logger.debug("Address: #{address}")
 
-      if zip_code
-        cached_forecast = Rails.cache.read(zip_code)
-
-        if cached_forecast
-          @forecast = cached_forecast
-          @from_cache = true
-        else
-          @forecast = fetch_forecast_from_api(zip_code)
-          Rails.cache.write(zip_code, @forecast, expires_in: 30.minutes)
-          @from_cache = false
-        end
+    if address.present?
+      geocode_result = geocode_address(address)
+      if geocode_result["postal_code"].present?
+        @postal_code = geocode_result["postal_code"]
+        cache_key = @postal_code
       else
-        render status: :unprocessable_entity, json: { error: 'Unable to geocode address' }
+        @latitude = geocode_result["latitude"]
+        @longitude = geocode_result["longitude"]
+        cache_key = "#{@latitude},#{@longitude}"
+      end
+
+      cached_forecast = Rails.cache.read(cache_key)
+
+      if cached_forecast
+        @forecast = cached_forecast
+        @from_cache = true
+      else
+        @forecast = fetch_forecast_from_api(cache_key)
+        Rails.cache.write(cache_key, @forecast, expires_in: 30.minutes)
+        @from_cache = false
+      end
+
+      @address = address
+      @zip_code = cache_key
+
+      if @forecast.nil?
+        render :enter_address
+      else
+        render :show
       end
     else
-      render template: 'weather/enter_address'
+      render :enter_address
     end
   end
-
-  private
 
   def geocode_address(address)
     api_key = ENV['GEOCODING_API_KEY']
     response = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{address}&key=#{api_key}")
-
-    Rails.logger.debug("Geocoding API request URL: https://maps.googleapis.com/maps/api/geocode/json?address=#{address}&key=#{api_key}")
     Rails.logger.debug("Geocoding API response code: #{response.code}")
     Rails.logger.debug("Geocoding API response body: #{response.body}")
 
     if response.success?
       results = JSON.parse(response.body)["results"]
       if results.any?
-        location = results.first["geometry"]["location"]
-        "#{location['lat']},#{location['lng']}"
-      else
-        Rails.logger.debug("Geocoding API returned no results")
-        nil
+        location = results[0]["geometry"]["location"]
+        postal_code_component = results[0]["address_components"].find { |comp| comp["types"].include?("postal_code") }
+        postal_code = postal_code_component ? postal_code_component["long_name"] : nil
+        return {
+          "latitude" => location["lat"],
+          "longitude" => location["lng"],
+          "postal_code" => postal_code
+        }
       end
-    else
-      Rails.logger.debug("Geocoding API request failed")
-      nil
     end
+    { "latitude" => nil, "longitude" => nil, "postal_code" => nil }
   end
-
   def fetch_forecast_from_api(location)
     api_key = ENV['WEATHER_API_KEY']
-    response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?lat=#{location.split(',').first}&lon=#{location.split(',').last}&appid=#{api_key}&units=imperial")
+    lat, lon = location.split(",")
+    response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{lon}&appid=#{api_key}&units=imperial")
+
     if response.success?
       {
         temperature: response['main']['temp'],
@@ -59,7 +73,7 @@ class WeatherController < ApplicationController
         low: response['main']['temp_min']
       }
     else
-      { error: "Unable to fetch weather data" }
+      nil
     end
-  end
+    end
 end
--- spec/controllers/weather_controller_spec.rb
+++ spec/controllers/weather_controller_spec.rb
@@ -2,56 +2,69 @@ require 'rails_helper'
 
 RSpec.describe WeatherController, type: :controller do
   describe 'GET #show' do
-    let(:address) { '1600 Amphitheatre Parkway, Mountain View, CA' }
-    let(:lat_lng) { '37.4224764,-122.0842499' }
-    let(:forecast) { { temperature: 72, high: 75, low: 68 } }
-
-    before do
-      allow(controller).to receive(:geocode_address).and_return(lat_lng)
-      allow(controller).to receive(:fetch_forecast_from_api).and_return(forecast)
-    end
-
     context 'when address is provided' do
+      let(:address) { '1600 Amphitheatre Parkway, Mountain View, CA' }
+      let(:geocoded_location) { { 'latitude' => 37.4224764, 'longitude' => -122.0842499, 'postal_code' => '94043' } }
+      let(:forecast_data) { { temperature: 72.0, high: 75.0, low: 68.0 } }
+
+      before do
+        allow(controller).to receive(:geocode_address).with(address).and_return(geocoded_location)
+      end
+
+      it 'returns a success response' do
+        get :show, params: { address: address }
+        expect(response).to be_successful
+        expect(assigns(:address)).to eq(address)
+        expect(assigns(:zip_code)).to eq('94043')
+      end
+
       context 'when caching is enabled' do
         before do
-          allow(Rails.cache).to receive(:read).with(lat_lng).and_return(nil)
-          allow(Rails.cache).to receive(:write).with(lat_lng, forecast, expires_in: 30.minutes)
+          Rails.cache.clear
         end
 
         it 'caches the forecast data' do
+          allow(controller).to receive(:fetch_forecast_from_api).and_return(forecast_data)
+          expect(Rails.cache).to receive(:write).with('94043', forecast_data, expires_in: 30.minutes)
           get :show, params: { address: address }
-          expect(Rails.cache).to have_received(:write).with(lat_lng, forecast, expires_in: 30.minutes)
         end
 
         it 'returns a success response' do
+          Rails.cache.write('94043', forecast_data, expires_in: 30.minutes)
           get :show, params: { address: address }
           expect(response).to be_successful
+          expect(assigns(:from_cache)).to be(true)
         end
 
         it 'fetches forecast data' do
+          expect(controller).to receive(:fetch_forecast_from_api).and_return(forecast_data)
           get :show, params: { address: address }
-          expect(assigns(:forecast)).to eq(forecast)
+          expect(assigns(:forecast)).to eq(forecast_data)
         end
       end
 
       context 'when caching is disabled' do
         before do
-          allow(Rails.cache).to receive(:read).with(lat_lng).and_return(nil)
+          Rails.cache.clear
         end
 
         it 'does not use the cache' do
+          allow(controller).to receive(:fetch_forecast_from_api).and_return(forecast_data)
           get :show, params: { address: address }
-          expect(Rails.cache).to have_received(:read).with(lat_lng)
+          expect(assigns(:from_cache)).to be(false)
         end
 
         it 'returns a success response' do
+          allow(controller).to receive(:fetch_forecast_from_api).and_return(forecast_data)
           get :show, params: { address: address }
           expect(response).to be_successful
+          expect(assigns(:forecast)).to eq(forecast_data)
         end
 
         it 'fetches forecast data' do
+          expect(controller).to receive(:fetch_forecast_from_api).and_return(forecast_data)
           get :show, params: { address: address }
-          expect(assigns(:forecast)).to eq(forecast)
+          expect(assigns(:forecast)).to eq(forecast_data)
         end
       end
     end
@@ -59,46 +72,59 @@ RSpec.describe WeatherController, type: :controller do
     context 'when address is not provided' do
       it 'renders the enter_address template' do
         get :show
-        expect(response).to render_template('enter_address')
+        expect(response).to render_template(:enter_address)
       end
     end
   end
 
   describe '#geocode_address' do
     it 'returns latitude and longitude for a given address' do
-      response_body = {
-        "results" => [
+      address = '1600 Amphitheatre Parkway, Mountain View, CA'
+      response_double = double('response', success?: true, code: 200, body: {
+        results: [
           {
-            "geometry" => {
-              "location" => {
-                "lat" => 37.4224764,
-                "lng" => -122.0842499
+            geometry: {
+              location: {
+                lat: 37.4224764,
+                lng: -122.0842499
               }
-            }
+            },
+            address_components: [
+              { long_name: '94043', types: ['postal_code'] }
+            ]
           }
         ]
-      }.to_json
+      }.to_json)
 
-      response_double = double('HTTParty::Response', success?: true, code: 200, body: response_body, parsed_response: JSON.parse(response_body))
       allow(HTTParty).to receive(:get).and_return(response_double)
-      expect(controller.send(:geocode_address, '1600 Amphitheatre Parkway, Mountain View, CA')).to eq('37.4224764,-122.0842499')
+
+      geocoded_location = controller.geocode_address(address)
+
+      expect(geocoded_location['latitude']).to eq(37.4224764)
+      expect(geocoded_location['longitude']).to eq(-122.0842499)
+      expect(geocoded_location['postal_code']).to eq('94043')
     end
   end
 
   describe '#fetch_forecast_from_api' do
     it 'returns forecast data for a given location' do
-      response_body = {
-        "main" => {
-          "temp" => 72,
-          "temp_max" => 75,
-          "temp_min" => 68
+      location = '37.4224764,-122.0842499'
+      response_double = double('response', success?: true, body: {
+        main: {
+          temp: 72.0,
+          temp_max: 75.0,
+          temp_min: 68.0
         }
-      }.to_json
+      }.to_json)
 
-      response_double = double('HTTParty::Response', success?: true, body: response_body, parsed_response: JSON.parse(response_body))
-      allow(response_double).to receive(:[]).with('main').and_return(response_double.parsed_response['main'])
       allow(HTTParty).to receive(:get).and_return(response_double)
-      expect(controller.send(:fetch_forecast_from_api, '37.4224764,-122.0842499')).to eq(temperature: 72, high: 75, low: 68)
+      allow(response_double).to receive(:[]).with('main').and_return(JSON.parse(response_double.body)['main'])
+
+      forecast = controller.fetch_forecast_from_api(location)
+
+      expect(forecast[:temperature]).to eq(72.0)
+      expect(forecast[:high]).to eq(75.0)
+      expect(forecast[:low]).to eq(68.0)
     end
   end
 end
